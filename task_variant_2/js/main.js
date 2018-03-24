f6daf9316d980c5bf027ff45713898a887f42c94
Bohdan Kisarchuk <angruh14@gmail.com>
2018-03-24 13:00:33 +0200

    create

---
+++ task_variant_2/js/main.js
@@ -0,0 +1,11 @@
+$(".cub").mouseenter( function () {
+        $(".cub3").animate({
+            left: '46%',top: '44'
+        }, 1000);
+    }
+);
+
+$(".cub1").mouseenter( function () {
+	$(".cub3").css("visibility", "visible");
+}
+);
